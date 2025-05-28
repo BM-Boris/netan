711521d66c593423125e45cbac14a9a941b5647e
BM-Boris <bb.minasenko@gmail.com>
2025-05-28 19:06:50 -0400

    Reorder urls: build-network before include('network.urls')

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,8 @@ from django.urls import path, include
 from network.views import BuildNetworkView
 
 urlpatterns = [
+    
+    path('api/build-network/', BuildNetworkView.as_view(), name='build-network'),
     path('api/', include('network.urls')),
     path('admin/', admin.site.urls),
-    path('api/build-network/', BuildNetworkView.as_view(), name='build-network'),
 ]
