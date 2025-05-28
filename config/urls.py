de88d440268a979e2359fb9089cc3171baa27535
BM-Boris <bb.minasenko@gmail.com>
2025-05-28 18:16:23 -0400

    rodin

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,10 @@
 # config/urls.py
+from django.contrib import admin
 from django.urls import path, include
+from network.views import BuildNetworkView
 
 urlpatterns = [
     path('api/', include('network.urls')),
+    path('admin/', admin.site.urls),
+    path('api/build-network/', BuildNetworkView.as_view(), name='build-network'),
 ]
